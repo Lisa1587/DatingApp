b01a53b9f25573797a520d9e9dfa1b893e7e97e2
Lisa <erasmusl@fourier.co.za>
2024-05-28 12:37:09 +0200

    End of section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -20,8 +20,8 @@ export class AccountService {
       map((responce: User )=>{
         const user = responce;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+         
+          this.setCurrentUser(user);
 
         }
 
@@ -35,8 +35,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map(user => {
         if (user) {
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
         
       })
@@ -44,6 +43,7 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    localStorage.setItem('user',JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
